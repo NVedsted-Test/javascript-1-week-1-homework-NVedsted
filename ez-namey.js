8dde6db0d0a547763788e6226c5d99ffd6c6ac0e
Nicklas Vedsted <nicklasvedsted@gmail.com>
2023-11-19 20:15:26 +0100

    Implement Ez-Namey

--- ez-namey.js
+++ ez-namey.js
@@ -1,7 +1,15 @@
 // Optional: Ez Namey (Startup name generator)
 
 function randomStartupName() {
-    // TODO: your code!
+    const firstWords = ["Easy", "Awesome", "Corporate", "Fantastic", "Smart", "Delightful", "Artificial"];
+    const secondWords = ["Mechanics", "Technology", "Incorporated", "Intelligence", "Learning", "Management"];
+
+    const randomFirstWordIndex = Math.floor(Math.random() * firstWords.length);
+    const randomFirstWord = firstWords[randomFirstWordIndex];
+    const randomSecondWordIndex = Math.floor(Math.random() * secondWords.length);
+    const randomSecondWord = secondWords[randomSecondWordIndex];
+
+    return `${randomFirstWord} ${randomSecondWord}`;
 }
 
 console.log(randomStartupName());
